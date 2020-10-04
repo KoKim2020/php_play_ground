d945763ee1be5f6b27dffb3ce680803e8e47ac01
KoKim <waiyanavionics@gmail.com>
2020-10-04 12:38:07 +0630

    status added

---
+++ greeting.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * - Static methods can be 
+ * called directly - without creating an instance of a class.
+ * 
+ * - Static methods are declared with the static keyword:
+ */
+
+
+
+?>
