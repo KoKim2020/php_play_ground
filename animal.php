94743382eddf140ac3fa18ca217c13b3fea06bee
KoKim <waiyanavionics@gmail.com>
2020-10-04 10:15:45 +0630

    modified

--- animal.php
+++ animal.php
@@ -1,8 +1,8 @@
 <?php
-    /**
-     * Interfaces make it easy to use a variety of different classes in the same way
-     */
     interface Animal {
         public function makeSound();
     }
+    /**
+     * class တွေ အများကြီးက action တခုတည်းကို ဘုံသုံး ချင်ရင် interfaces ကိုသုံး
+     */
 ?>
