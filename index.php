adb33c068552ebd8e8f142cd377f5a3d2a0429a1
KoKim <waiyanavionics@gmail.com>
2020-10-04 10:08:48 +0630

    interfaces added

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php 
-    include('goodbye.php');
-    // echo GoodBye::LEAVING_MESSAGE; // access constant outside the class
-    $goodbye = new GoodBye();
-    $goodbye->byebye();
+    include('animal.php');
+    //There are actions that all of the animals can do, but each animal does it in its own way.
+    class Cat implements Animal {
+        public function makeSound (){
+            echo "ညောင် ညောင်";
+        }
+    }
+
+    class Dog implements Animal {
+        public function makeSound()
+        {
+            echo " Bark ";
+        }
+    }
+
+    class Mouse implements Animal {
+        public function makeSound()
+        {
+            echo " Squeak ";
+        }
+    }
+
+    $cat = new Cat();
+    $dog = new Dog();
+    $mouse = new Mouse();
+    $animals = array($cat, $dog, $mouse);
+    foreach ($animals as $key => $value) {
+        $value->makeSound();
+        echo '<br>';
+    }
 ?>
