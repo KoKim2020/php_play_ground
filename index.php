e572f35c4eeb9b592c6038d93c335f0a4d3082c6
KoKim <waiyanavionics@gmail.com>
2020-10-10 12:24:19 +0630

    give alias to class name

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php 
     // namespace Html;
-    use Html as H; //Give a namespace an alias:
+    // use Html as H; //Give a namespace an alias:
+    use Html\Table as T;
+    use Html\Row as R; //Give a class an alias:
 
     include('table.php');
     // table instance
     // $table = new Html\Table();
-    $table = new H\Table();
+    $table = new T();
     $table->title = 'My Table';
     
     // row instance
     // $row = new Html\Row();
-    $row = new H\Row();
+    $row = new R();
     $row->numCells = 5;
 
 
