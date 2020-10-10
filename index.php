338d165ac639af7749e1abaff7a16ecda2fe6bee
KoKim <waiyanavionics@gmail.com>
2020-10-10 12:47:30 +0630

    array data type

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
-    
+    /**
+     * iterables is the datatype of parameters of function
+     * 
+     */
+    // use for array datatype
+    function Test( iterable $a) { // access modifiers is not used
+        foreach ($a as $key => $value) {
+            echo $value;
+        }
+    }
+
+    $k = Test([1,2,3]);
+    print_r($k);
 ?>
